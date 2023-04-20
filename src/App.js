a53faee6add32090abf44a2fd2f1c22fc87f8520
FatimaLobo <95919304+FatimaLobo@users.noreply.github.com>
2023-04-20 12:27:49 +0200

    Modify index page (#2)

--- src/App.js
+++ src/App.js
@@ -6,17 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <code>
+         Hello Wooooooooorld! 
+        </code>
+        <h2>Welcome to reelKeeper</h2>
+        <h3>We're under construction</h3>
+          
       </header>
     </div>
   );
