7289420262d5eae2ed0391f9e4e014b1a67e3c6f
FatimaLobo <95919304+FatimaLobo@users.noreply.github.com>
2023-04-27 19:07:50 +0200

    add favs, remove favs and local storage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
+import './inicio.css';
 import React, { useEffect, useState } from 'react';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import SearchBox from './components/SearchBox';
 import AddFavourites from './components/AddFavourites';
+import RemoveFavourites from './components/RemoveFavourites';
 import Header from './components/Header';
-
+import Carousel from './components/Carousel';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import Inicio from './components/Inicio';
+global.jQuery = $;
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -27,15 +35,35 @@ useEffect(()=>{
   getMovieRequest(searchValue);
 }, [searchValue]);
 
+useEffect(() => {
+  const movieFavourites = JSON.parse(localStorage.getItem('react-movie-app-favs'));
+setFavourites(movieFavourites);
+}, []);
+
+const saveToLocalStorage = (items) => {
+  localStorage.setItem('react-movie-app-favs', JSON.stringify(items));
+}
+
 const addFavouriteMovie = (movie) => {
   const newFavouriteList = [...favourites, movie];
   setFavourites(newFavouriteList);
-  console.log(newFavouriteList);
+  saveToLocalStorage(newFavouriteList);
+  
 }
+const removeFavouriteMovie = (movie) => {
+  const newFavouriteList = favourites.filter((favourite)=>favourite.imdbID !== movie.imdbID);
+  setFavourites(newFavouriteList);
+  saveToLocalStorage(newFavouriteList);
+}
+
+
+
   return (
     
       <div className="container-fluid movie-app">
         <Header/>
+        <Inicio/>
+        <Carousel/>
         
         <div className='row d-flec align-items-center mt-4 mb-4'>
           <MovieListHeading heading="Movies"/>
@@ -55,8 +83,8 @@ const addFavouriteMovie = (movie) => {
         <div className='row'>
           <MovieList 
           movies={favourites} 
-          handleFavouritesClick={addFavouriteMovie} 
-          favouriteComponent={AddFavourites}/>
+          handleFavouritesClick={removeFavouriteMovie} 
+          favouriteComponent={RemoveFavourites}/>
           </div>
       </div>
    
