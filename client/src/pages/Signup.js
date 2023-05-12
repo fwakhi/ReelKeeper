834b4c8807ab1f489d058a486e1853ce9a75fb13
Fatima Lobo <fatima.lobo.arroyo@gmail.com>
2023-05-12 13:37:11 +0200

    Connect Signup UI with server

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -7,11 +7,11 @@ import styles from "../style/Background.module.css"
 import '../style/Login.css';
 import axios from "axios";
 
-const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]){8,24}$/;
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
-const URL = 'http://localhost:8000/register';
+const URL = 'http://localhost:8000/signup';
 
 
 const Login = () => {
@@ -86,14 +86,13 @@ const Login = () => {
         }
         try {
             const response = await axios.post(URL,
-                JSON.stringify({ user, password }),
+                JSON.stringify({ user, password, email }),
                 {
                     headers: { 'Content-Type': 'application/json' },
-                    withCredentials: true
                 }
             );
             console.log(response?.data);
-            console.log(response?.accessToken);
+            // console.log(response?.accessToken);
             console.log(JSON.stringify(response))
             setSuccess(true);
             //clear state and controlled inputs
@@ -107,156 +106,151 @@ const Login = () => {
             } else if (err.response?.status === 409) {
                 setErrorMsg('Username Taken');
             } else {
-                setErrorMsg('Registration Failed')
+                setErrorMsg('Registration Failed', err.message)
             }
             errorRef.current?.focus();
         }
     }
 
-    return (
-        <>
-            {success ? (
-                <section>
-                    <h1>Success!</h1>
+    const signupContent =
+        (<>
+            <Card className="col-5 mx-auto shadow p-3 mb-5 bg-white rounded min-width">
+                <Card.Body>
+                    <Card.Title>Sign up</Card.Title><Card.Subtitle className="mb-3">Please enter your user and password!</Card.Subtitle><p ref={errorRef} className={errorMsg ? "errmsg" : "offscreeen"} aria-live="assertive">{errorMsg}</p><Form onSubmit={handleSubmit}>
+                        <Form.Group controlId="formBasicUsername">
+                            <Form.Label>Username:
+                                <span className={validUser ? "valid" : "hide"}>
+                                    <i className="fa-solid fa-check"></i>
+                                </span>
+                                <span className={validUser || !user ? "hide" : "invalid"}>
+                                    <i className="fa fa-times"></i>
+                                </span>
+                            </Form.Label>
+                            <Form.Control
+                                type="text"
+                                ref={userRef}
+                                autoComplete="off"
+                                onChange={(e) => setUser(e.target.value)}
+                                value={user}
+                                required
+                                aria-invalid={validUser ? "false" : "true"}
+                                aria-describedby="uidnote"
+                                onFocus={() => setUserFocus(true)}
+                                onBlur={() => setUserFocus(false)} />
+                        </Form.Group>
+                        <p id="uidnote" className={userFocus && user && !validUser ? "instructions" : "offscreen"}>
+                            <i className="fa-solid fa-circle-info fa-fw" />
+                            4 to 24 characters.<br />
+                            Must begin with a letter.<br />
+                            Letters, numbers, underscores, hyphens allowed.<br />
+                        </p>
+
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email:
+                                <span className={validEmail ? "valid" : "hide"}>
+                                    <i className="fa-solid fa-check"></i>
+                                </span>
+                                <span className={validEmail || !email ? "hide" : "invalid"}>
+                                    <i className="fa fa-times"></i>
+                                </span>
+                            </Form.Label>
+                            <Form.Control
+                                type="text"
+                                ref={emailRef}
+                                autoComplete="off"
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}
+                                required
+                                aria-invalid={validEmail ? "false" : "true"}
+                                aria-describedby="emailnote"
+                                onFocus={() => setEmailFocus(true)}
+                                onBlur={() => setEmailFocus(false)} />
+                        </Form.Group>
+                        <p id="emailnote" className={emailFocus && email && !validEmail ? "instructions" : "offscreen"}>
+                            <i className="fa-solid fa-circle-info fa-fw" />
+                            4 to 24 characters.<br />
+                        </p>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password:
+                                <span className={validPassword ? "valid" : "hide"}>
+                                    <i className="fa-solid fa-check"></i>
+                                </span>
+                                <span className={validPassword || !password ? "hide" : "invalid"}>
+                                    <i className="fa fa-times"></i>
+                                </span>
+                            </Form.Label>
+                            <Form.Control
+                                type="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}
+                                required
+                                aria-invalid={validPassword ? "false" : "true"}
+                                aria-describedby="pwdnote"
+                                onFocus={() => setPasswordFocus(true)}
+                                onBlur={() => setPasswordFocus(false)} />
+                        </Form.Group>
+                        <p id="pwdnote" className={passwordFocus && password && !validPassword ? "instructions" : "offscreen"}>
+                            <i className="fa-solid fa-circle-info fa-fw" />
+                            8 to 24 characters.<br />
+                            Must include uppercase and lowercase letters, a number and a special character.<br />
+                            Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
+                        </p>
+
+                        <Form.Group controlId="formBasicPasswordConfirmation">
+                            <Form.Label>Confirm Password:
+                                <span className={validMatch && matchPassword ? "valid" : "hide"}>
+                                    <i className="fa-solid fa-check"></i>
+                                </span>
+                                <span className={validMatch || !matchPassword ? "hide" : "invalid"}>
+                                    <i className="fa fa-times"></i>
+                                </span>
+                            </Form.Label>
+                            <Form.Control
+                                type="password"
+                                onChange={(e) => setMatchPassword(e.target.value)}
+                                value={matchPassword}
+                                required
+                                aria-invalid={validMatch ? "false" : "true"}
+                                aria-describedby="confirmnote"
+                                onFocus={() => setMatchFocus(true)}
+                                onBlur={() => setMatchFocus(false)} />
+                        </Form.Group>
+                        <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
+                            <i className="fa-solid fa-circle-info fa-fw" />
+                            Must match the first password input field.
+                        </p>
+
+                        <Button variant="dark" type="submit" disabled={!!(!validUser || !validPassword || !validMatch)} className="mx-auto mt-3 mb-3" style={{ width: "100%" }}>Sign up</Button>
+
+                        <Link to="/login" className='btn-pass text-center'>Already registered? Sign in!</Link>
+                    </Form>
+                </Card.Body>
+            </Card>
+        </>)
+
+    const signupSuccessContent =
+        (<>
+            <Card className="col-5 mx-auto shadow p-3 mb-5 bg-white rounded min-width">
+                <Card.Body><section>
+                    <Card.Title>Success!</Card.Title>
                     <p>
                         <Link to="/login" className='btn-pass text-center'>Sign In</Link>
                     </p>
                 </section>
-            ) :
-                (<Container className="h-100 mt-5 ">
-                    <Row className="align-items-center h-100">
-                        <Card className="col-5 mx-auto shadow p-3 mb-5 bg-white rounded min-width">
-                            <Card.Body>
-                                <Card.Title>Sign up</Card.Title>
-                                <Card.Subtitle className="mb-3">Please enter your user and password!</Card.Subtitle>
-                                <p ref={errorRef} className={errorMsg ? "errmsg" : "offscreeen"} aria-live="assertive">{errorMsg}</p>
-                                <Form onSubmit={handleSubmit}>
-                                    <Form.Group controlId="formBasicUsername">
-                                        <Form.Label>Username:
-                                            <span className={validUser ? "valid" : "hide"}>
-                                                <i className="fa-solid fa-check"></i>
-                                            </span>
-                                            <span className={validUser || !user ? "hide" : "invalid"}>
-                                                <i className="fa fa-times"></i>
-                                            </span>
-                                        </Form.Label>
-                                        <Form.Control
-                                            type="text"
-                                            id="username"
-                                            ref={userRef}
-                                            autoComplete="off"
-                                            onChange={(e) => setUser(e.target.value)}
-                                            value={user}
-                                            required
-                                            aria-invalid={validUser ? "false" : "true"}
-                                            aria-describedby="uidnote"
-                                            onFocus={() => setUserFocus(true)}
-                                            onBlur={() => setUserFocus(false)}
-                                        />
-                                    </Form.Group>
-                                    <p id="uidnote" className={userFocus && user && !validUser ? "instructions" : "offscreen"}>
-                                        <i className="fa-solid fa-circle-info fa-fw" />
-                                        4 to 24 characters.<br />
-                                        Must begin with a letter.<br />
-                                        Letters, numbers, underscores, hyphens allowed.<br />
-                                    </p>
+                </Card.Body>
+            </Card>
+        </>)
 
-                                    <Form.Group controlId="formBasicEmail">
-                                        <Form.Label>Email:
-                                            <span className={validEmail ? "valid" : "hide"}>
-                                                <i className="fa-solid fa-check"></i>
-                                            </span>
-                                            <span className={validEmail || !email ? "hide" : "invalid"}>
-                                                <i className="fa fa-times"></i>
-                                            </span>
-                                        </Form.Label>
-                                        <Form.Control
-                                            type="text"
-                                            id="email"
-                                            ref={emailRef}
-                                            autoComplete="off"
-                                            onChange={(e) => setEmail(e.target.value)}
-                                            value={email}
-                                            required
-                                            aria-invalid={validEmail ? "false" : "true"}
-                                            aria-describedby="emailnote"
-                                            onFocus={() => setEmailFocus(true)}
-                                            onBlur={() => setEmailFocus(false)}
-                                        />
-                                    </Form.Group>
-                                    <p id="emailnote" className={emailFocus && email && !validEmail ? "instructions" : "offscreen"}>
-                                        <i className="fa-solid fa-circle-info fa-fw" />
-                                        4 to 24 characters.<br />
-                                    </p>
-
-                                    <Form.Group controlId="formBasicPassword">
-                                        <Form.Label>Password:
-                                            <span className={validPassword ? "valid" : "hide"}>
-                                                <i className="fa-solid fa-check"></i>
-                                            </span>
-                                            <span className={validPassword || !password ? "hide" : "invalid"}>
-                                                <i className="fa fa-times"></i>
-                                            </span>
-                                        </Form.Label>
-                                        <Form.Control
-                                            type="password"
-                                            id="password"
-                                            onChange={(e) => setPassword(e.target.value)}
-                                            value={password}
-                                            required
-                                            aria-invalid={validPassword ? "false" : "true"}
-                                            aria-describedby="pwdnote"
-                                            onFocus={() => setPasswordFocus(true)}
-                                            onBlur={() => setPasswordFocus(false)}
-                                        />
-                                    </Form.Group>
-                                    <p id="pwdnote" className={passwordFocus && password && !validPassword ? "instructions" : "offscreen"}>
-                                        <i className="fa-solid fa-circle-info fa-fw" />
-                                        8 to 24 characters.<br />
-                                        Must include uppercase and lowercase letters, a number and a special character.<br />
-                                        Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
-                                    </p>
-
-                                    <Form.Group controlId="formBasicPasswordConfirmation">
-                                        <Form.Label>Confirm Password:
-                                            <span className={validMatch && matchPassword ? "valid" : "hide"}>
-                                                <i className="fa-solid fa-check"></i>
-                                            </span>
-                                            <span className={validMatch || !matchPassword ? "hide" : "invalid"}>
-                                                <i className="fa fa-times"></i>
-                                            </span>
-                                        </Form.Label>
-                                        <Form.Control
-                                            type="password"
-                                            id="confirm_password"
-                                            onChange={(e) => setMatchPassword(e.target.value)}
-                                            value={matchPassword}
-                                            required
-                                            aria-invalid={validMatch ? "false" : "true"}
-                                            aria-describedby="confirmnote"
-                                            onFocus={() => setMatchFocus(true)}
-                                            onBlur={() => setMatchFocus(false)}
-                                        />
-                                    </Form.Group>
-                                    <p id="confirmnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
-                                        <i className="fa-solid fa-circle-info fa-fw" />
-                                        Must match the first password input field.
-                                    </p>
-
-                                    <button disabled={!validUser || !validPassword || !validMatch ? true : false}>Sign Up</button>
-
-                                    {/* <Button variant="dark" type="submit" className="mx-auto mt-3 mb-3" style={{ width: "100%" }}>Sign up</Button> */}
-
-                                    <Link to="/login" className='btn-pass text-center'>Already registered? Sign in!</Link>
-                                </Form>
-                            </Card.Body>
-                        </Card>
-                    </Row>
-                </Container>
-                )}
+    return (
+        <>
+            (<Container className="h-100 mt-5 ">
+                <Row className="align-items-center h-100">
+                    {success ? signupSuccessContent : signupContent}
+                </Row>
+            </Container>
         </>
     )
 }
-
 
 export default Login;
--- server/index.js
+++ server/index.js
@@ -1,13 +1,30 @@
 import express from 'express'
 import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url';
 import db from './config/db.js'
 import userRoutes from './routes/routes.js'
+import registerRoutes from './routes/signup.js'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use('/users', userRoutes)
+app.use('/signup', registerRoutes)
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = path.dirname(__filename);
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
 
 try {
     db.authenticate()
