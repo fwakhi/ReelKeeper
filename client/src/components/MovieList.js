7c19440e2b45fc50f4912a884fe97be6fefe1eb6
Fatima Lobo <fatima.lobo.arroyo@gmail.com>
2023-05-28 20:05:12 +0200

    Add movie grid and fix credits

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { imgUrl } from '../api/tmdb'
 import { useNavigate } from 'react-router-dom';
 import Buttons from './Buttons';
+import { Button, Container, Row, Col, Image } from 'react-bootstrap';
+
 
 
 const MovieList = (props) => {
@@ -14,7 +16,7 @@ const MovieList = (props) => {
         <>
             {React.Children.toArray(props.movies.map((movie) =>
                 <div key={movie.id} className='image-container d-flex justify-content-start m-3'>
-                    <img onClick={() => handleClick(movie.id)} src={imgUrl + movie.poster_path} alt="poster" className='mb-1'></img>
+                    <Image className='mb-1 mt-4' src={imgUrl + movie.poster_path} onClick={() => handleClick(movie.id)} alt="poster" rounded />
                     {props.hideButtons ? "" : <Buttons movie={movie} />}
                 </div>
             ))}
