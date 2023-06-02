bf856b6d33db276fde85d59026aa8f8f979d1b09
Fatima Lobo <fatima.lobo.arroyo@gmail.com>
2023-06-02 13:59:45 +0200

    Remove movies from lists

--- client/src/components/ListButton.js
+++ client/src/components/ListButton.js
@@ -1,6 +1,4 @@
-import React, { useState } from "react";
-import useAuth from "../hooks/useAuth";
-
+import React, { useEffect, useState } from "react";
 import { saveMovieList, removeMovieList } from '../api/services/MovieList'
 import useInfo from "../hooks/useInfo";
 import { refreshUser } from "../api/axios";
@@ -9,35 +7,40 @@ const ListButton = (props) => {
 
     const movie = props.movie;
 
-    const [listId, setListId] = useState('');
     const { userInfo, setUserInfo } = useInfo();
+    const [listsMap, setListsMap] = useState([]);
+
+    useEffect(() => {
+        mapMoviesInList();
+    }, [movie, userInfo])
 
     const addMovieList = async (listId, movie) => {
         if (movie && listId && await saveMovieList(listId, movie)) {
-            setUserInfo(await refreshUser(userInfo?.id));
-            setListId(listId);
+            setUserInfo(await refreshUser(userInfo.id));
         }
     }
 
     const deleteMovieList = async (listId, movie) => {
         if (movie && listId && await removeMovieList(listId, movie)) {
-            setUserInfo(await refreshUser(userInfo?.id));
+            setUserInfo(await refreshUser(userInfo.id));
         }
     }
 
-    if (userInfo?.lists?.movielists?.find(m => m.id == movie.id && m.listId == listId)) {
-        return (
-            <>
-                <li className="nav-item dropdown">
-                    <button className="nav-link dropdown-toggle btn listButton p-2" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
-                        <i className="fa-regular fa-plus" style={{ color: "#1f1f1f" }}></i>
-                    </button>
-                    <div className="dropdown-menu text-left profileDropdown" aria-labelledby="navbarDropdown">
-                        {userInfo?.lists?.map((list) => <li className="dropdown-item px-2" value={list.id} key={list.id} onClick={() => deleteMovieList(list.id, movie)}>{list.title}</li>)}
-                    </div>
-                </li>
-            </>
-        )
+    const mapMoviesInList = () => {
+        const list = userInfo?.lists?.map(list => { { return { id: list.id, contains: list.movielists.find(k => k.id == movie.id) != null } } })
+        setListsMap(list);
+    }
+
+    const isMovieInList = (listId) => {
+        if (listsMap?.length == 0) {
+            return false
+        }
+        const found = listsMap?.find(list => list.id == listId && list.contains);
+        return found != null;
+    }
+
+    const handleClick = async (listId, movie) => {
+        isMovieInList(listId) ? await deleteMovieList(listId, movie) : await addMovieList(listId, movie);
     }
 
     return (
@@ -48,7 +51,7 @@ const ListButton = (props) => {
                 </button>
 
                 <div className="dropdown-menu text-left profileDropdown" aria-labelledby="navbarDropdown">
-                    {userInfo?.lists?.map((list) => <li className="dropdown-item px-2" value={list.id} key={list.id} onClick={() => addMovieList(list.id, movie)}>{list.title}</li>)}
+                    {userInfo?.lists?.map((list) => <li className={`dropdown-item px-2 ${isMovieInList(list.id) ? "item-selected" : ""}`} value={list.id} key={list.id} onClick={() => handleClick(list.id, movie)}>{list.title}</li>)}
                 </div>
             </li>
 
